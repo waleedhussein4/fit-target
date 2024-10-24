ac52f9e47e577fcf263c92ec908a970dfb107dd7
Antoine Medawar <antoinemedawar004@gmail.com>
2024-10-25 00:48:22 +0300

    Sign Up button activated + Sign Up activity design

--- src/main/java/com/example/fittarget/MainActivity.java
+++ src/main/java/com/example/fittarget/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.fittarget;
 import android.content.Intent;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -35,6 +36,13 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
+    Button signUpButton = findViewById(R.id.btnSignUp);
+    signUpButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+            startActivity(intent);
+        }
+    });
     }
 }
